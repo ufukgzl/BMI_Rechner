1b7753a5a78086f97132023efa05420d0c842f22
Ufuk Güzel <ufukgzl@web.de>
2025-04-06 14:43:48 +0200

    Code Completed

--- bmi_project.py
+++ bmi_project.py
@@ -16,51 +16,38 @@ label_weight.grid(row=1, column=0)
 entry_weight = Entry(width=10)
 entry_weight.grid(row=1, column=1)
 
-
+result_label = Label(text="")
+result_label.grid(row=3, column=0)
 
 def calculator():
-    height = float(entry_height.get())
-    weight = float(entry_weight.get())
-    bmi = weight / (height/100)**2
+    if entry_height.get() == "" or entry_weight.get() == "":
+        result_label.config(text="Please Enter Valid Number")
 
 
-    if bmi < 16.0:
-        label_bmi_category = Label(text="Your BMI Category is: Severely Underweight")
-        label_bmi_category.grid(row=3, column=0)
-        label_bmi = Label(text="Your BMI is: " + str(bmi))
-        label_bmi.grid(row=2, column=0)
-    elif 16.0 <= bmi < 18.5:
-        label_bmi_category = Label(text="Your BMI Category is: Underweight")
-        label_bmi_category.grid(row=3, column=0)
-        label_bmi = Label(text="Your BMI is: " + str(bmi))
-        label_bmi.grid(row=2, column=0)
-    elif 18.5 <= bmi < 25:
-        label_bmi_category = Label(text="Your BMI Category is: Normal")
-        label_bmi_category.grid(row=3, column=0)
-        label_bmi = Label(text="Your BMI is: " + str(bmi))
-        label_bmi.grid(row=2, column=0)
-
-    elif 25 <= bmi < 30:
-        label_bmi_category = Label(text="Your BMI Category is: Overweight")
-        label_bmi_category.grid(row=3, column=0)
-        label_bmi = Label(text="Your BMI is: " + str(bmi))
-        label_bmi.grid(row=2, column=0)
-    elif 30 <= bmi < 35:
-        label_bmi_category = Label(text="Your BMI Category is: Moderately Obese")
-        label_bmi_category.grid(row=3, column=0)
-        label_bmi = Label(text="Your BMI is: " + str(bmi))
-        label_bmi.grid(row=2, column=0)
-    elif 35 <= bmi < 40:
-        label_bmi_category = Label(text="Your BMI Category is: Severely Obese")
-        label_bmi_category.grid(row=3, column=0)
-        label_bmi = Label(text="Your BMI is: " + str(bmi))
-        label_bmi.grid(row=2, column=0)
     else:
-        label_bmi_category = Label(text="Your BMI Category is: Morbidly Obese")
-        label_bmi_category.grid(row=3, column=0)
-        label_bmi = Label(text="Your BMI is: " + str(bmi))
-        label_bmi.grid(row=2, column=0)
+        try:
+            height = float(entry_height.get())
+            weight = float(entry_weight.get())
+            bmi = weight / (height/100)**2
 
+            if bmi < 16.0:
+                result_label.config(text = f"Your BMI is: {str(bmi)}\nYour BMI Category is: Severely Underweight")
+            elif 16.0 <= bmi < 18.5:
+                result_label.config(text = f"Your BMI is: {str(bmi)}\nYour BMI Category is: Underweight")
+            elif 18.5 <= bmi < 25:
+                result_label.config(text = f"Your BMI is: {str(bmi)}\nYour BMI Category is: Normal")
+            elif 25 <= bmi < 30:
+                result_label.config(text = f"Your BMI is: {str(bmi)}\nYour BMI Category is: Overweight")
+            elif 30 <= bmi < 35:
+                result_label.config(text = f"Your BMI is: {str(bmi)}\nYour BMI Category is: Moderately Obese")
+            elif 35 <= bmi < 40:
+                result_label.config(text = f"Your BMI is: {str(bmi)}\nYour BMI Category is: Severely Obese")
+            else:
+                result_label.config(text=f"Your BMI is: {str(bmi)}\nYour BMI Category is: Morbidly Obese")
+
+
+        except ValueError:
+                result_label.config(text="Please Enter Valid Number")
 
 label_bmi_button = Button(text= "Calculate BMI", command=calculator)
 label_bmi_button.grid(row=5, column=0)
